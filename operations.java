e00841bf9454ec98708ed47dee876d121f18ba6e
Madalena Ye <madalena@pc-198-62.customer.ask4.lan>
2022-09-28 01:30:40 +0100

    Adicionei coisas fixes pq sou fixe

--- operations.java
+++ operations.java
@@ -2,8 +2,20 @@ import java.util.Scanner;
 public class operations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String operation = scanner.nextLine();
         int a = scanner.nextInt();
         int b = scanner.nextInt();
+        if (operation.equals("sum")){
         System.out.println(a + b);
     }
+        else if (operation.equals("mul")){
+            System.out.println(a * b);
+        }
+        else if (operation.equals("div")){
+            System.out.println(a / b);
+        }
+        else if (operation.equals("sub")){
+            System.out.println(a - b);
+        }
+        }
 }
